425b54925c09cd829e082f90534fe28f2937dd0d
superkllerdarkassasin999rus <95570330+superkllerdarkassasin999rus@users.noreply.github.com>
2022-03-13 14:30:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let phrases = [
 { text: 'поиграть в фоллаут', image: 'https://overclockers.ru/st/legacy/blog/378513/169853_O.png' },
-{ text: 'а подумать как купить схему на броню секретной службы в фоллаут', image: 'https://fallout76.su/images/bronya/bronya_sekretnoy_sluzhby_fallout_resizebyheight_cropcenter_500_250.jpg' },
+{ text: 'подумать как купить схему на броню секретной службы в фоллаут', image: 'https://fallout76.su/images/bronya/bronya_sekretnoy_sluzhby_fallout_resizebyheight_cropcenter_500_250.jpg' },
 { text: 'разобраться, как самому делать билды', image: 'https://steamuserimages-a.akamaihd.net/ugc/1745687931296383926/0BBF60386BCC3B88F2E959E7DF8C71B9DE12660D/' },
-{ text: 'а посмотреть стресс бороду', image: 'https://i.ytimg.com/vi/Myz4wY2Hqh4/maxresdefault.jpg' },
+{ text: 'посмотреть стресс бороду', image: 'https://i.ytimg.com/vi/Myz4wY2Hqh4/maxresdefault.jpg' },
 { text: 'поставить пупсов на полку', image: 'https://cdn.shazoo.ru/134367_4RuFzBElP5_bobbleheads.jpg' },
 { text: 'посмотреть цену на fallout 1st', image: 'https://gamebomb.ru/files/galleries/001/d/dd/329875.jpg' },
 { text: 'посмотреть трейлер обновления и ужаснуться в комментариях', image: 'https://www.global-esports.news/wp-content/uploads/2022/02/The-events.jpg' },
